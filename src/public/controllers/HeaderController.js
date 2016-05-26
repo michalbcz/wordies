f1d77a01dee1eaccbef75289367f0f9a821cf3a4
michalbcz <michal@bernhard.cz>
2016-05-26 19:08:26 +0200

    added getting of current user name

--- src/public/controllers/HeaderController.js
+++ src/public/controllers/HeaderController.js
@@ -1,7 +1,24 @@
-app.controller("HeaderController", ['$scope', function($scope) {
-    $scope.userName = "Michal Bernhard"
+app.controller(
+      "HeaderController", ['$scope', '$rootScope', '$http',
+      function($scope, $rootScope, $http) {
+        $scope.username = "n/a"
 
-    $scope.$on('loginSuccessful', function(event, data) {
-       alert("Login successfull event called! Data:" + data);
-    })
+        $scope.$on('loginSuccessful', function(event, data) {
+
+           $http({
+             method: 'GET',
+             url: 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
+             headers: { 'Authorization' : 'Bearer ' + $scope.accessToken }
+           }).then(function success(response) {
+                 console.log(response);
+                 var data = response.data;
+                 var id = data.id;
+                 var username = data.name;
+                 $scope.username = username;
+                 $rootScope.id = id;
+             }, function error(response) {
+                 console.error("error!", response);
+             });
+
+      });
 }]);
---
+++ src/public/index.js
@@ -0,0 +1,67 @@
+console.debug("index.js script");
+
+var app = angular.module("slovicka", ['ngRoute']);
+
+app.config(["$routeProvider", function($routeProvider) {
+    console.debug("config of routeProvider", $routeProvider, this);
+
+    $routeProvider
+        .when('/login', {
+          controller: 'LoginController',
+          templateUrl: '/views/login.html'
+        })
+        .otherwise({
+          controller: 'TranslateController',
+          templateUrl: "/views/main.html"
+        });
+
+}]);
+
+app.run(['$rootScope', '$location', '$http',
+  function($rootScope, $location, $http) {
+
+    // see what's going on when the route tries to change
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+      // next is an object that is the route that we are starting to go to
+      // current is an object that is the route where we are currently
+      var currentPath = $location.path();
+      console.log("current path:", currentPath);
+      var pattern = /.*access_token=(.*?)\&/i
+      var accessTokenMatcher = pattern.exec(currentPath);
+      console.log(accessTokenMatcher)
+      if (accessTokenMatcher.length > 0) {  // it matches
+          var accessToken = accessTokenMatcher[1];
+      }
+
+      $rootScope.accessToken = accessToken;
+      $rootScope.$broadcast('loginSuccessful', "login vole");
+
+    });
+  }
+]);
+
+app.factory("myInterceptor", ["$log", function($log) {
+    $log.debug("log from regresp responseInterceptor");
+
+    return {
+        request: function(request) {
+            $log.debug("request is ($@@#@): ", request);
+            return request;
+        },
+        response: function(response) {
+            $log.debug("response is ($@@#@): ", response);
+            return response;
+        },
+        requestError: function(error) {
+            $log.debug("request error is: ($@@#@): ", error);
+        },
+        responseError: function(error) {
+            $log.debug("request error is: ($@@#@): ", error);
+        }
+    }
+}]);
+
+app.config(["$httpProvider", function($httpProvider) {
+    console.debug("config of httpProvider", this);
+    $httpProvider.interceptors.push('myInterceptor');
+}]);
